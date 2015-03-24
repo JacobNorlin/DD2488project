d903bdbd11dbffc075af814b441109859058cbaa
jacob <jacob.norlinandersson@gmail.com>
2015-03-24 17:08:28 +0100

    More work on the printer

--- src/main/scala/koolc/ast/Printer.scala
+++ src/main/scala/koolc/ast/Printer.scala
@@ -11,6 +11,9 @@ object Printer {
 
     sb.append("object "+main.id+" { \n")
     sb.append("\t def main() : Unit = { \n")
+    printStatements(main.stats)
+    sb.append("}\n")
+    sb.append("}")
 
 
     def printStatements(statements:List[StatTree]) = {
@@ -46,7 +49,7 @@ object Printer {
 
         case Assign => {
           val assignNode = statement.asInstanceOf[Assign]
-          sb.append(assignNode.id+" = ")
+          sb.append(assignNode.id.value+" = ")
           printExpression(assignNode.expr)
           sb.append(";\n")
           ""
@@ -62,7 +65,7 @@ object Printer {
 
         case ArrayAssign => {
           val arrayNode = statement.asInstanceOf[ArrayAssign]
-          sb.append(arrayNode.id+"[")
+          sb.append(arrayNode.id.value+"[")
           printExpression(arrayNode.index)
           sb.append("] = ")
           printExpression(arrayNode.expr)
@@ -84,7 +87,8 @@ object Printer {
     def printExpressions(expressions:List[ExprTree]) = {
 
       for(expression <- expressions){
-
+        printExpression(expression)
+        sb.append("\n")
       }
 
     }
@@ -92,15 +96,169 @@ object Printer {
     def printExpression(expression:ExprTree):String = {
       expression.getClass match {
         case Plus => {
-          var plusNode = expression.asInstanceOf[Plus]
+          val plusNode = expression.asInstanceOf[Plus]
+          sb.append("(")
           sb.append(printExpression(plusNode.lhs))
           sb.append(" + ")
           sb.append(printExpression(plusNode.rhs))
+          sb.append(")")
           ""
         }
+        case Minus => {
+          val minusNode = expression.asInstanceOf[Minus]
+          sb.append("(")
+          sb.append(printExpression(minusNode.lhs))
+          sb.append(" - ")
+          sb.append(printExpression(minusNode.rhs))
+          sb.append(")")
+          ""
+        }
+        case Times => {
+          val timesNode = expression.asInstanceOf[Times]
+          sb.append("(")
+          sb.append(printExpression(timesNode.lhs))
+          sb.append(" * ")
+          sb.append(printExpression(timesNode.rhs))
+          sb.append(")")
+          ""
+        }
+        case Div => {
+          val divNode = expression.asInstanceOf[Div]
+          sb.append("(")
+          sb.append(printExpression(divNode.lhs))
+          sb.append(" * ")
+          sb.append(printExpression(divNode.rhs))
+          sb.append(")")
+          ""
+        }
+        case And => {
+          val andNode = expression.asInstanceOf[And]
+          sb.append("(")
+          sb.append(printExpression(andNode.lhs))
+          sb.append(" && ")
+          sb.append(printExpression(andNode.rhs))
+          sb.append(")")
+          ""
+        }
+        case Or => {
+          val orNode = expression.asInstanceOf[Or]
+          sb.append("(")
+          sb.append(printExpression(orNode.lhs))
+          sb.append("||")
+          sb.append(printExpression(orNode.rhs))
+          sb.append(")")
+          ""
+        }
+        case LessThan => {
+          val ltNode = expression.asInstanceOf[LessThan]
+          sb.append("(")
+          sb.append(printExpression(ltNode.lhs))
+          sb.append(" < ")
+          sb.append(printExpression(ltNode.rhs))
+          sb.append(")")
+          ""
+        }
+        case Equals => {
+          val equalsNode = expression.asInstanceOf[Equals]
+          sb.append("(")
+          sb.append(printExpression(equalsNode.lhs))
+          sb.append(" == ")
+          sb.append(printExpression(equalsNode.rhs))
+          sb.append(")")
+          ""
+        }
+        case ArrayRead => {
+          val arNode = expression.asInstanceOf[ArrayRead]
+          printExpression(arNode.arr)
+          sb.append("[")
+          printExpression(arNode.index)
+          sb.append("]")
+          ""
+        }
+        case ArrayLength => {
+          val alNode = expression.asInstanceOf[ArrayLength]
+          printExpression(alNode.arr)
+          sb.append(".length")
+          ""
+        }
+        case MethodCall => {
+          val mcNode = expression.asInstanceOf[MethodCall]
+          printExpression(mcNode.obj)
+          sb.append("."+mcNode.meth.value+"( ")
+          printExpressions(mcNode.args)
+          sb.append(" );")
+          ""
+        }
+        case IntLit => {
+          val intNode = expression.asInstanceOf[IntLit]
+          sb.append(intNode.value
+          ""
+        }
+        case StringLit => {
+          val strNode = expression.asInstanceOf[StringLit]
+          sb.append(strNode.value)
+          ""
+        }
+        case True => {
+          sb.append("true")
+          ""
+        }
+        case False => {
+          sb.append("false")
+          ""
+        }
+        case Identifier => {
+          val idNode = expression.asInstanceOf[Identifier]
+          sb.append(idNode.value)
+          ""
+        }
+        case This => {
+          sb.append("this")
+          ""
+        }
+        case NewIntArray => {
+          val niaNode = expression.asInstanceOf[NewIntArray]
+          sb.append("new int[")
+          printExpression(niaNode.size)
+          sb.append("]")
+          ""
+        }
+        case New =>{
+          val newNode = expression.asInstanceOf[New]
+          sb.append("new "+newNode.tpe.value+"()")
+          ""
+        }
+        case Not => {
+          val notNode = expression.asInstanceOf[Not]
+          sb.append("!")
+          printExpression(notNode.expr)
+          ""
+        }
+
       }
 
     }
 
+    def printType(nodeType: TypeTree) = nodeType.getClass match {
+      case IntType => {
+        sb.append("Int")
+      }
+      case BooleanType => {
+        sb.append("Bool")
+      }
+      case StringType => {
+        sb.append("String")
+      }
+      case IntArrayType => {
+        sb.append("Int[]")
+      }
+      case Identifier => {
+        val idNode = nodeType.asInstanceOf[Identifier]
+        sb.append(idNode.value)
+      }
+    }
+
+
+    sb.toString()
   }
 }
