259db0d7b258888fd4618b7bd0752a7ca3775a19
jacob <jacob.norlinandersson@gmail.com>
2015-04-07 12:17:53 +0200

    reverter nameanalysis.scala

--- src/main/scala/koolc/analyzer/NameAnalysis.scala
+++ src/main/scala/koolc/analyzer/NameAnalysis.scala
@@ -10,10 +10,6 @@ object NameAnalysis extends Pipeline[Program, Program] {
   def run(ctx: Context)(prog: Program): Program = {
     import ctx.reporter._
 
-    prog.classes.map(cls => {
-      val clsSym = 
-    })
-
     // Step 1: Collect symbols in declarations
     // Step 2: Attach symbols to identifiers (except method calls) in method bodies
     // (Step 3:) Print tree with symbol ids for debugging
