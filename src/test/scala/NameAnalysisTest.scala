394c7797cd88450da6f5e54537b446b34c3bbe31
jacob <jacob.norlinandersson@gmail.com>
2015-04-28 01:29:10 +0200

    Added some general testing to name analysis.

--- src/test/scala/NameAnalysisTest.scala
+++ src/test/scala/NameAnalysisTest.scala
@@ -10,6 +10,7 @@ import koolcNew.lexer.Lexer
 import koolcNew.utils.Context
 import org.scalatest._
 
+
 /**
  * Created by jacob on 2015-04-27.
  */
@@ -18,46 +19,111 @@ class NameAnalysisTest extends FlatSpec{
 
 
   "ourProg" should "be equal to refProg" in {
+
+    val testDir = new File("testprograms/valid").listFiles()
     val pipeline = Lexer andThen Parser andThen NameAnalysis
     val refPipeline = koolc.lexer.Lexer andThen koolc.ast.Parser andThen koolc.analyzer.NameAnalysis
-
-    val ctx = Context(reporter = new koolcNew.utils.Reporter(), file = new File("testprograms/lab3/valid/Factorial.kool"), outDir = None)
-    val refCtx = koolc.utils.Context(reporter = new koolc.utils.Reporter(), files = List(new File("testprograms/lab3/valid/Factorial.kool")), outDir = None)
-    val program = pipeline.run(ctx)(ctx.file)
-    val refProgram = refPipeline.run(refCtx)(ctx.file)
+    for(file <- testDir){
 
 
-    val ids = getAllIds(program)
-    val refIds = getAllIds(refProgram)
-    println(ids, refIds)
+      val ctx = Context(reporter = new koolcNew.utils.Reporter(), file = file, outDir = None)
+      val refCtx = koolc.utils.Context(reporter = new koolc.utils.Reporter(), files = List(new File("testprograms/lab3/valid/Factorial.kool")), outDir = None)
+      val program = pipeline.run(ctx)(ctx.file)
+      val refProgram = refPipeline.run(refCtx)(ctx.file)
 
 
-    assert(program.toString === koolc.ast.ASTPrinter(refProgram))
-    assert(ids === refIds)
+      val ids = getAllIds(program)
+      val refIds = getAllIds(refProgram)
+      val names = getAllNames(program)
+      val refNames = getAllNames(refProgram)
+      println(file.getName+": "+names, file.getName+": "+refNames)
+      println(file.getName+": "+names, file.getName+": "+refNames)
+
+
+      //assert(program.toString === koolc.ast.ASTPrinter(refProgram))
+      assert(ids === refIds)
+      assert(names === refNames)
+
+    }
+
 
   }
+  def getAllNames(prog: Program): List[String]= {
+    def getArgs(met: MethodDecl): List[String] = {
+      if (met.args != null) {
+        met.args.map(arg => arg.getSymbol.name)
+      } else {
+        List()
+      }
+    }
+    prog.classes.flatMap(cls => {
+      List(cls.getSymbol.name) ++
+        cls.vars.map(v => v.getSymbol.name) ++
+        cls.methods.flatMap(met => {
+          List(met.getSymbol.name) ++
+            getArgs(met) ++
+            met.vars.map(v1 => v1.getSymbol.name)
+        })
+    }).sortBy(x => x)
+  }
+
+  def getAllNames(prog: koolc.ast.Trees.Program): List[String]= {
+    def getArgs(met: koolc.ast.Trees.MethodDecl): List[String] = {
+      if (met.args != null) {
+        met.args.map(arg => arg.getSymbol.name)
+      } else {
+        List()
+      }
+    }
+    prog.classes.flatMap(cls => {
+      List(cls.getSymbol.name) ++
+        cls.vars.map(v => v.getSymbol.name) ++
+        cls.methods.flatMap(met => {
+          List(met.getSymbol.name) ++
+            getArgs(met) ++
+            met.vars.map(v1 => v1.getSymbol.name)
+        })
+    }).sortBy(x => x)
+  }
+
 
   def getAllIds(prog: Program): List[Int]= {
+    def getArgs(met:MethodDecl):List[Int] = {
+      if(met.args != null){
+        met.args.map(arg => arg.getSymbol.id)
+      }else{
+        List()
+      }
+    }
     prog.classes.flatMap(cls => {
-      List(cls.getSymbol.id)++
+      List(cls.getSymbol.id) ++
         cls.vars.map(v => v.getSymbol.id) ++
         cls.methods.flatMap(met => {
-          List(met.getSymbol.id)++
-            met.args.map(arg => arg.getSymbol.id) ++
+          List(met.getSymbol.id) ++
+            getArgs(met) ++
             met.vars.map(v1 => v1.getSymbol.id)
         })
-    })
+    }).sortBy(x => x)
+
   }
   def getAllIds(prog: koolc.ast.Trees.Program): List[Int] = {
+    def getArgs(met: koolc.ast.Trees.MethodDecl): List[Int] = {
+      if (met.args != null) {
+        met.args.map(arg => arg.getSymbol.id)
+      } else {
+        List()
+      }
+    }
     prog.classes.flatMap(cls => {
-      List(cls.getSymbol.id)++
+      List(cls.getSymbol.id) ++
         cls.vars.map(v => v.getSymbol.id) ++
         cls.methods.flatMap(met => {
-          List(met.getSymbol.id)++
-            met.args.map(arg => arg.getSymbol.id) ++
+          List(met.getSymbol.id) ++
+          getArgs(met) ++
             met.vars.map(v1 => v1.getSymbol.id)
         })
-    })
+    }).sortBy(x => x)
+
   }
 
 
